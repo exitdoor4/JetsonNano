8805849bae5ae6ca994f7061fdce7ec79792e7a5
exitdoor4 <exitdoor4@naver.com>
2024-02-23 17:20:07 +0900

    add : code

--- 7-Segment/lib/DAVIDBackpack.h
+++ 7-Segment/lib/DAVIDBackpack.h
@@ -1,22 +1,91 @@
 #ifndef _DAHT16K33_H
 #define _DAHT16k33_H
 
+#include <cstddef> // NULL, size_t 사용
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h> // 하드웨어의 제어와 상태정보를 얻음
+#include <cstdlib> // exit(), abort(), atexit()
+#include <cstdio> // c언어의 stdio.h와 같음
+#include <fcntl.h> // 파일을 열고, 잠금 및 다른 작업을 할 수 있음
+#include <unistd.h> // 다양한 필수 POSIX 함수와 상수 read, write
+#include <errno.h>
+#include <stdint.h> // uint8_t 같은 자료형을 사용 가능
 
 
 
+#define DEC 10
+#define HEX 16
+#define OCT 8
+#define BIN 2
+#define BYTE 0
+
+#define HT16K33_BLINK_CMD 0x80
+#define HT16K33_BLINK_DISPLAYON 0x01
+#define HT16K33_BLINK_OFF 0
+#define HT16K33_BLINK_2HZ  1
+#define HT16K33_BLINK_1HZ  2
+#define HT16K33_BLINK_HALFHZ  3
+
+#define HT16K33_CMD_BRIGHTNESS 0xE0
+
+class HT16K33
+{
+public:
+    unsigned char kI2CBus ;         // I2C bus of the HT16K33
+    int kI2CFileDescriptor ;        // File Descriptor to the HT16K33
+    int kI2CAddress ;               // Base address of HT16K33; defaults to 0x70
+    int error ;
+    HT16K33(int address=0x70);
+    ~HT16K33() ;
+    uint16_t displayBuffer[10];
+
+    bool openHT16K33() ;
+    void closeHT16K33();
+    int getError() ;
+
+    int i2cwrite(int writeValue) ;
+
+    void begin();
+    void end() ;
+    void setBrightness(uint8_t b);
+    void blinkRate(uint8_t b);
+    int writeDisplay(void);
+    void clear(void);
+
+   void init(uint8_t a);
 
 
+    size_t write(uint8_t c);
 
+     void print(char, int = BYTE);
+     void print(unsigned char, int = BYTE);
+     void print(int, int = DEC);
+     void print(unsigned int, int = DEC);
+     void print(long, int = DEC);
+     void print(unsigned long, int = DEC);
+     void print(double, int = 2);
+     void println(char, int = BYTE);
+     void println(unsigned char, int = BYTE);
+     void println(int, int = DEC);
+     void println(unsigned int, int = DEC);
+     void println(long, int = DEC);
+     void println(unsigned long, int = DEC);
+     void println(double, int = 2);
+     void println(void);
 
+     void writeDigitRaw(uint8_t x, uint8_t bitmask);
+     void writeDigitNum(uint8_t x, uint8_t num, bool dot = false);
+     void drawColon(bool state);
+     void printNumber(long, uint8_t = 2);
+     void printFloat(double, uint8_t = 2, uint8_t = DEC);
+     void printError(void);
 
+     void writeColon(void);
 
+     uint8_t position;
 
-
-
-
-
-
-
+    
+};
 
 
 #endif
--- 7-Segment/src/main.cpp
+++ 7-Segment/src/main.cpp
@@ -4,3 +4,116 @@
 #include <termios.h>
 #include <time.h>
 #include "DAVIDBackpack.h"
+
+int getkey() {
+    int character;
+    struct termios orig_term_attr;
+    struct termios new_term_attr;
+
+    /* set the terminal to raw mode */
+    tcgetattr(fileno(stdin), &orig_term_attr);
+    memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
+    new_term_attr.c_lflag &= ~(ECHO|ICANON);
+    new_term_attr.c_cc[VTIME] = 0;
+    new_term_attr.c_cc[VMIN] = 0;
+    tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
+
+    /* read a character from the stdin stream without blocking */
+    /*   returns EOF (-1) if no character is available */
+    character = fgetc(stdin);
+
+    /* restore the original terminal attributes */
+    tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
+
+    return character;
+}
+
+int main(){
+    HT16K33 *displayMatrix = new HT16K33() ;
+    // Default I2C Bus 1
+    int err = displayMatrix -> openHT16K33();
+    if(err <0){
+        printf("error: %d", displayMatrix->error);
+    } else {
+        printf("HT16K33 Device Address: 0x%02X\n", displayMatrix->kI2CAddress);
+        printf("HT16K33 Device Bus: 0x%02X\n", displayMatrix->kI2CBus);
+        displayMatrix->begin() ;
+        displayMatrix->clear();
+
+        displayMatrix->writeDigitNum(0,1);
+        displayMatrix->writeDigitNum(1,2);
+        displayMatrix->writeDigitNum(2,3);
+        displayMatrix->writeDigitNum(3,4);
+
+        displayMatrix->writeDisplay();
+
+        //try to print a number thats the too long
+        // show dashes & blink
+        displayMatrix->print(10000,DEC);
+        displayMatrix->writeDisplay();
+        displayMatrix->blinkRate(HT16K33_BLINK_2HZ);
+        sleep(5);
+
+        displayMatrix->blinkRate(HT16K33_BLINK_OFF);
+
+        displayMatrix->print(0XBEEF,HEX);
+        displayMatrix->writeDisplay();
+        sleep(5);
+
+        displayMatrix->printFloat(0.586, 3, DEC);
+        displayMatrix->writeDisplay();
+        sleep(5);
+
+     printf("Starting Count Down, Engines On\n") ;
+        // print with print/println
+        for (int counter = 10; counter >= 0; counter--) {
+           displayMatrix->println(counter);
+           displayMatrix->writeDisplay();
+           sleep(1);
+        }
+    printf("Hit ESC key to exit\n");
+        // Minutes and seconds - Start a 9:50 to show an exciting time change at 10:00!
+        uint16_t counter = 590 ;
+        bool drawDots = false ;
+        while(displayMatrix->error >= 0 && getkey() != 27){
+                // display as hours and minutes
+                int hours = counter / 60 ;
+                int minutes = counter % 60 ;
+                // printf("Counter: %d\n", counter);
+                displayMatrix->writeDigitNum(0, (hours / 10), drawDots);
+                displayMatrix->writeDigitNum(1, hours  % 10, drawDots);
+                displayMatrix->drawColon(true);
+                displayMatrix->writeDigitNum(3, (minutes / 10) % 10, drawDots);
+                displayMatrix->writeDigitNum(4, minutes % 10, drawDots);
+                displayMatrix->writeDisplay() ;
+                counter ++ ;
+                sleep(1) ;
+        }        displayMatrix->print(10000, DEC);
+        displayMatrix->writeDisplay();
+        displayMatrix->blinkRate(HT16K33_BLINK_2HZ) ;
+        sleep(5) ;
+
+        displayMatrix->blinkRate(HT16K33_BLINK_OFF) ;
+
+        // print a hex number
+        displayMatrix->print(0xBEEF, HEX);
+        displayMatrix->writeDisplay();
+        sleep(5);
+
+        // print a floating point number
+        displayMatrix->printFloat(0.596,3,DEC);
+        displayMatrix->writeDisplay();
+        sleep(5);
+
+        printf("Starting Count Down, Engines On\n") ;
+        // print with print/println
+        for (int counter = 10; counter >= 0; counter--) {
+           displayMatrix->println(counter);
+           displayMatrix->writeDisplay();
+           sleep(1);
+        }
+
+    }
+    displayMatrix->end() ;
+    displayMatrix->closeHT16K33();
+}
